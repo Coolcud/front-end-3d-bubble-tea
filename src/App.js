8e1cd4d444322bb514a401496e04b548793693be
Sage Choi <coolcuds@gmail.com>
2023-07-24 11:26:43 -0700

    Add initial jsx for headers and submitting orders

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>3d Bubble Tea</header>
+      <main>
+        <h1>Boba Order</h1>
+        <div className='order-body'>
+          <button className='btn btn-primary'>Show drink!</button>
+        </div>
+      </main>
     </div>
   );
 }
