a4f862f0d30db9387754bc77ff39d345131ce9ee
Sage Choi <coolcuds@gmail.com>
2023-07-24 20:57:45 -0700

    Convert form option data from json to js

---
+++ src/data/FormOptions.js
@@ -0,0 +1,54 @@
+export const formOptions = {
+  bases: [
+    "Signature",
+    "Royal",
+    "Assam",
+    "Red oolong",
+    "High mt. oolong",
+    "Rock",
+    "Jasmine",
+    "Rose green",
+    "Iron buddha",
+    "Iron buddha plus",
+    "Earl grey",
+    "Mango green",
+    "Buckwheat green",
+    "Black sugar puerh",
+    "Chai latte",
+    "Matcha latte",
+    "Hojicha latte",
+    "Genmaicha latte",
+    "Rooibos",
+    "Black sugar boba milk",
+    "Coffee milk tea",
+    "Lava boba milk"
+  ],
+  toppings: [
+    "Pearl (boba)",
+    "Red bean",
+    "Honey",
+    "Lychee jelly",
+    "Ai-yu jelly",
+    "Coffee jelly",
+    "Chia seeds",
+    "Black sugar jelly",
+    "Pudding",
+    "Grass jelly"
+  ],
+  sweetness: [
+    "More sugar",
+    "Standard",
+    "80%",
+    "50%",
+    "30%",
+    "No sugar"
+  ],
+  ice: [
+    "More ice",
+    "Standard",
+    "80%",
+    "50%",
+    "30%",
+    "No ice"
+  ]
+};
