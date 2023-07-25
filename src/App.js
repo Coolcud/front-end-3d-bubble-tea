dad49d9e37c89751f11f402ae8b9c50e8d2a9ae6
Sage Choi <coolcuds@gmail.com>
2023-07-25 16:30:45 -0700

    Add canvas and model

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
 import NewOrderForm from './components/NewOrderForm';
+
+import Model from './components/Model';
 
 function App() {
   return (
@@ -7,6 +11,13 @@ function App() {
       <header>3d Bubble Tea</header>
       <main>
         <h1>Boba Order</h1>
+        <div>
+          <Canvas>
+            <Suspense fallback={null}>
+              <Model />
+            </Suspense>
+          </Canvas>
+        </div>
         <div className='order-body'>
           <NewOrderForm></NewOrderForm>
         </div>
