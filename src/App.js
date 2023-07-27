a37678ab136eb4d5dcab424a592b4b78d4ac0afe
Sage Choi <coolcuds@gmail.com>
2023-07-26 19:14:46 -0700

    Add axios and api calls for adding orders to db

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import './App.css';
-import { useEffect, useState, Suspense } from "react";
 import axios from 'axios';
+import { useState, useEffect, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import NewOrderForm from './components/NewOrderForm';
-
 import Model from './components/Model';
 import { OrbitControls } from '@react-three/drei';
 
-const API = process.env.TEA_API_URL;
+const API = "https://threed-bubble-tea-api.onrender.com/orders";
 
 function App() {
+  const [orderData, setOrderData] = useState([]);
+
+  const getOrders = () => {
+    axios
+      .get(API)
+      .then((response) => {
+        // setOrderData(response.data);
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log('Error:', error);
+        alert('Unable to retrieve orders.');
+      });
+  };
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
+  const postOrder = (newOrder) => {
+    axios
+      .post(API, newOrder)
+      .then(() => {
+        getOrders();
+      })
+      .catch((error) => {
+        console.log('Error:', error);
+        alert('Unable to create a new order.');
+      });
+  };
+
   return (
     <div className="App">
       <header>3d Bubble Tea</header>
@@ -30,7 +60,7 @@ function App() {
           </div>
           <div className='flex-child form'>
             <div className='form-body'>
-              <NewOrderForm></NewOrderForm>
+              <NewOrderForm orders={orderData} addOrder={postOrder}/>
             </div>
           </div>
         </div>
