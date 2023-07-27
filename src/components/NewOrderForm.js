4dcf3412de47b3e2a55caa515ea0f3a09365b66a
Sage Choi <coolcuds@gmail.com>
2023-07-26 19:15:44 -0700

    Add hooks and useeffects for each formfield, and disable submit until form filled out

--- src/components/NewOrderForm.js
+++ src/components/NewOrderForm.js
@@ -1,26 +1,28 @@
+import PropTypes from "prop-types";
 import React, { useState, useEffect } from "react";
 import { formOptions } from "../data/FormOptions";
 
-const NewOrderForm = () => {
+const INITIAL_FORM_DATA = {
+  base: '',
+  toppings: [],
+  sweetness: '',
+  temp: ''
+}
+
+const NewOrderForm = ({ orders, addOrder }) => {
+  // Store and set form field values
+  const [formFields, setFormFields] = useState(INITIAL_FORM_DATA);
+  const [baseValue, setBaseValue] = useState("default base");
+  const [toppingsValues, setToppingsValues] = useState([]);
+  const [sweetValue, setSweetValue] = useState("default sweet");
+  const [tempValue, setTempValue] = useState("default temp");
+
   // Array of booleans to track topping checkboxes
   const [checkedState, setCheckedState] = useState(
     new Array(formOptions.toppings.length).fill(false)
   );
 
-  const [formFields, setFormFields] = useState({
-    base: '',
-    toppings: [],
-    temp: '',
-    sweetness: ''
-  });
-
-  // Set state base based on user selection
-  const onBaseChange = (event) => {
-    setFormFields({
-      ...formFields,
-      base: event.target
-    })
-  }
+//~~~~~~~~~~~~~~~~~~~~~~BASE FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~
 
   // Display each base in the dropdown menu
   const baseComponents = formOptions.bases.map((base, index) => {
@@ -30,6 +32,20 @@ const NewOrderForm = () => {
       </option>
     );
   });
+
+  // Set state base based on user selection
+  const onBaseChange = (event) => {
+    setBaseValue(event.target.value);
+  };
+  
+  useEffect((formFields) => {
+    setFormFields({
+      ...formFields,
+      base: baseValue
+    });
+  }, [baseValue]);
+
+//~~~~~~~~~~~~~~~~~~~~~~TOPPINGS FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~
 
   // Display each topping in a list of checkboxes
   const toppingComponents = formOptions.toppings.map((topping, index) => {
@@ -53,7 +69,7 @@ const NewOrderForm = () => {
       index === position ? !checkbox : checkbox
     );
       
-    setCheckedState(updatedCheckedState)
+    setCheckedState(updatedCheckedState);
   };
 
   // Add topping of checked boxes to list and update state with effect
@@ -66,46 +82,135 @@ const NewOrderForm = () => {
         console.log(toppingsList)
       }
     }
+
+    setToppingsValues(toppingsList);
     setFormFields({
       ...formFields,
       toppings: toppingsList
-    })
-  }, [checkedState])
+    });
+  }, [checkedState]);
 
-  // Set state base based on user selection
-  const onBaseChange = (event) => {
-    setFormFields({
-      ...formFields,
-      base: event.target
-    })
-  }
+//~~~~~~~~~~~~~~~~~~~~~~SWEETNESS FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~
 
-  // Display each base in the dropdown menu
-  const baseComponents = formOptions.bases.map((base, index) => {
+  // Display each sugar level in the dropdown menu
+  const sweetnessComponents = formOptions.sweetness.map((sugar, index) => {
     return (
-      <option key={index} name={base} value={base}>
-        {base}
+      <option key={index} name={sugar} value={sugar}>
+        {sugar}
       </option>
     );
   });
 
+  // Set state sweetness based on user selection
+  const onSweetnessChange = (event) => {
+    setSweetValue(event.target.value);
+  };
+
+  useEffect((formFields) => {
+    setFormFields({
+      ...formFields,
+      sweetness: sweetValue
+    });
+  }, [sweetValue]);
+
+//~~~~~~~~~~~~~~~~~~~~~~TEMP FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~
+
+  // Display each temp in the dropdown menu
+  const tempComponents = formOptions.temps.map((temp, index) => {
+    return (
+      <option key={index} name={temp} value={temp}>
+        {temp}
+      </option>
+    );
+  });
+
+  // Set state temp based on user selection
+  const onTempChange = (event) => {
+    setTempValue(event.target.value);
+  };
+
+  useEffect((formFields) => {
+    setFormFields({
+      ...formFields,
+      temp: tempValue
+    });
+  }, [tempValue]);
+
+  useEffect(() => {
+
+  })
+  console.log("formFields:", formFields);
+
+//~~~~~~~~~~~~~~~~~~~~~~OTHER FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~
+  
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addOrder(formFields);
+    setFormFields(INITIAL_FORM_DATA);
+  }
+
+  const writePreview = () => {
+    let preview = '';
+
+    preview += `${baseValue} milk tea `;
+    preview += `with ${toppingsValues.join(', ')} toppings, `;
+    preview += `${sweetValue}, and `;
+    preview += tempValue;
+    preview = preview.toLowerCase();
+
+    return preview;
+  }
+
+//~~~~~~~~~~~~~~~~~~~~~~RETURN~~~~~~~~~~~~~~~~~~~~~~
+  const prev = writePreview();
   return (
-    <form>
+    <form className="new-order-form" onSubmit={handleSubmit}>
       <div>
         <label htmlFor="base">
           Select your drink base
-          <select onChange={onBaseChange}>
+          <select defaultValue={baseValue} onChange={onBaseChange}>
+            <option disabled hidden value="default base">--Choose a base--</option>
             {baseComponents}
           </select>
         </label>
       </div>
       <div>
-        <p>Select your toppings</p>
-        {toppingComponents}
+        <label htmlFor="toppings">
+          Select your toppings
+          {toppingComponents}
+        </label>
       </div>
-      <input type="submit" value="Submit Drink!"></input>
+      <div>
+        <label htmlFor="sweetness">
+          Select your sweetness
+          <select defaultValue={sweetValue} onChange={onSweetnessChange}>
+          <option disabled hidden value="default sweet">--Choose a sugar level--</option>
+            {sweetnessComponents}
+          </select>
+        </label>
+      </div>
+      <div>
+        <label htmlFor="temp">
+          Select your ice level
+          <select defaultValue={tempValue} onChange={onTempChange}>
+          <option disabled hidden value="default temp">--Choose an ice level--</option>
+            {tempComponents}
+          </select>
+        </label>
+      </div>
+      <p>Preview: {prev}</p>
+      <input
+        disabled={writePreview().includes("default")}
+        type="submit"
+        value="Submit Drink!"
+      />
     </form>
   );
+};
+
+NewOrderForm.propTypes = {
+  addOrder: PropTypes.func.isRequired
 };
 
 export default NewOrderForm;
