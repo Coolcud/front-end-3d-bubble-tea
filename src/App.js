059ff11d6fb007eb7bd595bc3520484995478918
Sage Choi <coolcuds@gmail.com>
2023-07-31 15:49:37 -0700

    Add alt implementation of $2 model as Scene

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 import axios from 'axios';
-import { useState, useEffect, Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
 import NewOrderForm from './components/NewOrderForm';
-import Model from './components/Model';
 import { OrbitControls } from '@react-three/drei';
+import { useState, useEffect, Suspense } from "react";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
+import OldModel from './components/OldModel';
+import Model from './components/Model';
 
 const API = process.env.REACT_APP_TEA_API_URL;
+
+// Renders $2 model with mtl and obj files in public/
+// Requires <Scene /> not <Model /> in App to view
+// function Scene(props) {
+//   const materials = useLoader(MTLLoader, 'Cup of drink.mtl');
+//   const obj = useLoader(OBJLoader, 'Cup of drink.obj', (loader) => {
+//     materials.preload();
+//     loader.setMaterials(materials);
+//   });
+
+//   console.log(obj);
+//   return <primitive object={obj} scale={0.4} {...props}/>
+// };
+
 
 function App() {
   const [orderData, setOrderData] = useState([]);
@@ -46,18 +63,16 @@ function App() {
       <main>
         <h1>Boba Order</h1>
         <div className='flex-container'>
-          {/* <div className='flex-child-model'>
+          <div className='flex-child-model'>
             <div className='model-body' style={{ position: "relative", width: 950, height: 1000 }}>
               <Canvas>
                 <Suspense fallback={null}>
-                  <ambientLight intensity={0.5} />
-                  <directionalLight position={[10, 10, 5]} intensity={6} />
                   <OrbitControls />
                   <Model />
                 </Suspense>
               </Canvas>
             </div>
-          </div> */}
+          </div>
           <div className='flex-child form'>
             <div className='form-body'>
               <NewOrderForm addOrder={postOrder}/>
